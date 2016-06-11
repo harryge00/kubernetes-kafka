d464075e9b6570dc7eed2c02155dbae2e7fc4312
harryge00 <harryge00@gmail.com>
2016-06-12 02:07:57 +0800

    cloudTrancInc

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,5 @@
 bin/kafka-topics.sh --create --zookeeper zoo1:2181 --replication-factor 1 --partitions 1 --topic test
 
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+
+bin/kafka-console-consumer.sh --zookeeper zoo1:2181 --topic test --from-beginning
