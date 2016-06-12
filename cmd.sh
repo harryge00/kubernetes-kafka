70c6e703ea6acf10422a4ee587bba66bfce412ee
harryge00 <harryge00@gmail.com>
2016-06-13 00:51:04 +0800

    config for abac test

--- cmd.sh
+++ cmd.sh
@@ -2,8 +2,10 @@ bin/kafka-topics.sh --create --zookeeper zoo1:2181 --replication-factor 1 --part
 bin/kafka-topics.sh --zookeeper zoo1:2181 --list
 
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic mytopic
+bin/kafka-console-producer.sh --broker-list kafka-server-2:9092 --topic test
 
 bin/kafka-console-consumer.sh --zookeeper zoo1:2181 --topic test --from-beginning
+bin/kafka-console-consumer.sh --zookeeper zookeeper-server-1:2181 --topic test --from-beginning
 
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic mytopic
 
@@ -16,3 +18,5 @@ bin/kafka-console-producer.sh --broker-list 103.21.118.223:9092 --topic test
 bin/kafka-console-consumer.sh --zookeeper zoo1:2181 --topic mytopic --from-beginning
 
 bin/kafka-topics.sh --describe --zookeeper zoo1:2181 --topic mytopic
+bin/kafka-topics.sh --describe --zookeeper zookeeper-server-1:2181 --topic mytopic
+bin/kafka-topics.sh --describe --zookeeper zookeeper-server-1:2181 --topic test
